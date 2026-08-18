ffd436e2717b5444769495a4df4e8c2cdd8bc809
GiovanniSandovalP <giovanni.sandoval.preciado@gmail.com>
2026-08-17 23:23:08 -0600

    fix: waypaper script errors fixed

--- scripts/theme/theme_manager.sh
+++ scripts/theme/theme_manager.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Extract the path name
-#WALLPAPER_PATH="$1"
-FILENAME=$(basename "WALLPAPER_PATH")
+WALLPAPER_PATH="$1"
+FILENAME=$(basename "$WALLPAPER_PATH")
 THEME_NAME="${FILENAME%.*}"
 
 # Dotfiles paths
@@ -11,7 +11,7 @@ WAYBAR_THEMES="$HOME/.config/waybar/themes"
 
 # Waybar
 if [ -f "$WAYBAR_THEMES/$THEME_NAME.css" ]; then
-    ln -snf "$WAYBAR_THEMES/$THEME_NAME.css" "WAYBAR_THEMES/current.css"
+    ln -snf "$WAYBAR_THEMES/$THEME_NAME.css" "$WAYBAR_THEMES/current.css"
     killall -SIGUSR2 waybar 2>/dev/null || (pkill waybar && waybar &)
 fi
 
