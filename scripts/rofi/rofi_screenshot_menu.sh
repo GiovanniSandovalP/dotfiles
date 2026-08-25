af27bac2f2eee26c4132643ed049b1f6c9dbf897
GiovanniSandovalP <giovanni.sandoval.preciado@gmail.com>
2026-08-25 00:12:12 -0600

    feat: update keybinds and rofi screenshot menu

--- scripts/rofi/rofi_screenshot_menu.sh
+++ scripts/rofi/rofi_screenshot_menu.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-opt_1="󰆞  Region (Edit)"
-opt_2="  Window (Copy)"
-opt_3="󰍹  Full Screen (Copy)"
+opt_1="󰆞  Region"
+opt_2="  Window"
+opt_3="󰍹  Full Screen"
 
 choice=$(echo -e "$opt_1\n$opt_2\n$opt_3" | rofi -dmenu -i -theme ~/.config/rofi/screenshot_menu.rasi)
 
@@ -13,10 +13,10 @@ case $choice in
         ;;
     "$opt_2")
         sleep 0.2
-        hyprshot -m window --clipboard-only
+        hyprshot -m window --raw | satty --filename -
         ;;
     "$opt_3")
         sleep 0.2
-        hyprshot -m output --clipboard-only
+        hyprshot -m output --raw | satty --filename -
         ;;
 esac
